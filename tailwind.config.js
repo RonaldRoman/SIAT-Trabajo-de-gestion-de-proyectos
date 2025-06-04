11e75c892b4fc2a550ec93770b8b49927d40e011
Josecass01 <josecastellanos274@gmail.com>
2025-06-03 21:58:59 -0500

    instalacion de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./PresentacionAtractivo/**/*.html", // ¡Importante para tus archivos HTML!
+    // Puedes añadir más rutas si es necesario, por ejemplo:
+    // "./src/**/*.{js,jsx,ts,tsx}", 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
